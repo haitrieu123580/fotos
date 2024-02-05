0b08a6135561bf867e40e0e443ad3cf230202107
haitrieu123580 <haitrieu123580@gmail.com>
2024-02-05 20:14:12 +0700

    init vite + shadcn

---
+++ front_end/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from '@/components/ui/button'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Button variant={'outline'} onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </>
+  )
+}
+
+export default App
