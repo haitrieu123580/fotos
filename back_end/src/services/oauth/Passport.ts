5c4978c9102be59b2965f079c2497052a295729b
haitrieu123580 <haitrieu123580@gmail.com>
2024-02-20 09:09:19 +0700

    save user to database

--- back_end/src/services/oauth/Passport.ts
+++ back_end/src/services/oauth/Passport.ts
@@ -6,6 +6,7 @@ import UserRepoInterface from "../../repositories/user/UserRepoInterface";
 import UserRepo from "../../repositories/user/UseRepo";
 dotenv.config();
 
+const userRepo: UserRepoInterface = new UserRepo();
 passport.use(
     new GoogleStrategy(
         {
@@ -13,20 +14,21 @@ passport.use(
             clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
             callbackURL: "/passport/google/callback",
         },
-        function (_accessToken: string, _refreshToken: string, profile: any, done: any) {
-            // store token and token into db
-            console.log('accessToken', _accessToken);
-            console.log('refreshToken', _refreshToken);
-            console.log('profile', profile);
-            // const userRepo: UserRepoInterface = new UserRepo();
-            // userRepo.createUser({
-            //     email: profile.emails[0].value,
-            //     name: profile.displayName,
-            //     password: '',
-            //     provider: 'google',
-            //     providerId: profile.id,
-            //     avatar: profile.photos[0].value
-            // });
+        async function (_accessToken: string, _refreshToken: string, profile: any, done: any) {
+            try {
+                const isExist = await userRepo.isExistedEmail(profile.emails[0].value);
+                if (isExist) {
+                    return done(null, profile);
+                }
+                await userRepo.createUser({
+                    email: profile.emails[0].value,
+                    username: profile.displayName,
+                    avatar: profile.photos[0].value
+                })
+            } catch (error) {
+                console.log(error)
+            }
+
             done(null, profile); //done is callback function
         }
     )
